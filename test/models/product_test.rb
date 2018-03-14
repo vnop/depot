47d23424c3e8c535d2d63cf29b0d8db5c290b88f
Vandeth Nop <vandeth@entelo.com>
2018-03-14 11:54:20 -0700

    Add catalog page logic and styles

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -32,10 +32,11 @@ class ProductTest < ActiveSupport::TestCase
   def new_product(image_url)
     Product.new(title: "My Book Title",
                 description: "yyy",
+                price: 1,
                 image_url: image_url)
   end
   test "image url" do
-    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.JPG
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
               http://a.b.c/x/y/z/fred.gif }
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
     ok.each do |name|
