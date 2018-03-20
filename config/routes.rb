dc5bc7646cdaa4f4a4135ab328f25be020b368c7
Vandeth Nop <vandeth@entelo.com>
2018-03-20 11:23:39 -0700

    Add i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,34 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
+
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
 
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
+  get "sessions/create"
+  get "sessions/destroy"
 
-  get 'store/index'
+  resources :users
+
   resources :products do
     get :who_bought, on: :member
   end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
+
+  # get 'store/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'store#index', as: 'store'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
